7ca32a2ab93549c7fc26db6685e908ffb5f12ab3
529562814@qq.com <529562814@qq.com>
2019-09-06 17:51:10 +0800

    second commit

--- src/pages/index/index.jsx
+++ src/pages/index/index.jsx
@@ -3,17 +3,32 @@ import { View, Button, Text } from '@tarojs/components'
 
 import './index.less'
 
+
+//api 
+import api from '../../constants/api';
+
+function getJSON(url,data){
+  return Taro.request({url:url,data:data,method:'GET'});
+}
+
+function postJSON(url,data){
+  return Taro.request({url:url,data:data,method:'POST'});
+}
+
+async function getTopicList(){
+  let result = await getJSON(api.getTopics).catch(message=>{
+    console.log("出错了，错误信息："+message);
+  });
+  console.log(result);
+  console.log('123');
+}
+
 //api
-import {getTopicList} from '../../utils/request'
 
 class Index extends Component {
 
     config = {
     navigationBarTitleText: '首页'
-  }
-
-  getJSON(url,data){
-    return Taro.request({url:url,data:data,method:'GET'});
   }
 
   componentWillReceiveProps (nextProps) {
@@ -25,10 +40,14 @@ class Index extends Component {
   }
 
   componentDidMount(){
-    getTopicList();
+    getTopicList().then(data=>{
+      console.log('取到的数据:'+data)
+    })
   }
 
-  componentDidShow () { }
+  componentDidShow () { 
+    
+  }
 
   componentDidHide () { }
 
--- src/utils/request.js
+++ src/utils/request.js
@@ -1,9 +1,10 @@
-// import Taro from '@tarojs/Taro';
+import Taro, { Component } from '@tarojs/Taro';
 import api from '../constants/api';
 
-// export function getJSON(url,data){
-//    return Taro.request({url:url,data:data,method:'GET'});
-// }
+export function getJSON(url,data){
+   return Taro.request({url:url,data:data,method:'GET'});
+}
+
 export async function getTopicList(){
   let result = await getJSON(api.getTopics).catch(message=>{
     console.log("出错了，错误信息："+message);
